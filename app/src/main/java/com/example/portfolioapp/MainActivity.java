3dcab7e934332eaddcc4f6676e386ab7c9cfdab1
Gaurav Rawat <gauravrawat@Gauravs-MacBook-Air.local>
2024-01-14 01:09:10 -0500

    portfolio app done

--- app/src/main/java/com/example/portfolioapp/MainActivity.java
+++ app/src/main/java/com/example/portfolioapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.portfolioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView list = findViewById(R.id.rv_project);
+
+        Project[] projects = {
+                new Project("Guru", "App for academy coaches, empowering them to effortlessly monitor and manage user activities with ease.", R.drawable.gurulogo),
+                new Project("PPR", "Allows multiple users to showcase and promote their academies, providing a platform for players to make bookings.", R.drawable.pnplogo),
+                new Project("BUDDY", "Sports activity app enabling users to seamlessly monitor and enhance their fitness journey.", R.drawable.buddylogo),
+                new Project("Photo_sharing_App", "App to post photos and write descriptions about it. Users can also follow other users.", R.drawable.photo_sharing_app),
+                new Project("ForRental", "Rental app allowing the landlord to rent their property on the app, allowing user to save or enquire about property.", R.drawable.forrental)
+        };
+
+
+        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+
+        list.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/example/portfolioapp/ProjectsAdapter.java
@@ -0,0 +1,57 @@
+package com.example.portfolioapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsAdapter.ProjectViewHolder> {
+
+    private Project[] projects;
+
+    public ProjectsAdapter(Project[] projects) {
+        this.projects = projects;
+    }
+
+    @NonNull
+    @Override
+    public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_project, parent, false);
+        return new ProjectViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
+
+        holder.bind(projects[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return projects.length;
+    }
+
+    static class ProjectViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView appImage;
+        TextView appTitle;
+        TextView appDescription;
+        public ProjectViewHolder(@NonNull View itemView) {
+            super(itemView);
+            appImage = itemView.findViewById(R.id.image_view_project_icon);
+            appDescription = itemView.findViewById(R.id.text_view_project_description);
+            appTitle = itemView.findViewById(R.id.text_view_project_title);
+        }
+
+        public void bind(Project project){
+            appTitle.setText(project.name);
+            appDescription.setText(project.description);
+            appImage.setImageResource(project.image);
+        }
+    }
+}
