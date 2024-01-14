b9d2cbe9456681ba799eda94add8e45eafa68e46
Gaurav Rawat <gauravrawat@Gauravs-MacBook-Air.local>
2024-01-14 01:54:24 -0500

    hydro calc added

--- app/src/main/java/com/example/portfolioapp/MainActivity.java
+++ app/src/main/java/com/example/portfolioapp/MainActivity.java
@@ -15,11 +15,13 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView list = findViewById(R.id.rv_project);
 
         Project[] projects = {
+                new Project("Photo_sharing_App", "App to post photos and write descriptions about it. Users can also follow other users.", R.drawable.photo_sharing_app),
+                new Project("ForRental", "Rental app allowing the landlord to rent their property on the app, allowing user to save or enquire about property.", R.drawable.forrental),
+                new Project("HydroCalculator", "Calculates hydro based on user input in kwh.", R.drawable.bolt),
                 new Project("Guru", "App for academy coaches, empowering them to effortlessly monitor and manage user activities with ease.", R.drawable.gurulogo),
                 new Project("PPR", "Allows multiple users to showcase and promote their academies, providing a platform for players to make bookings.", R.drawable.ppr),
                 new Project("BUDDY", "Sports activity app enabling users to seamlessly monitor and enhance their fitness journey.", R.drawable.buddy),
-                new Project("Photo_sharing_App", "App to post photos and write descriptions about it. Users can also follow other users.", R.drawable.photo_sharing_app),
-                new Project("ForRental", "Rental app allowing the landlord to rent their property on the app, allowing user to save or enquire about property.", R.drawable.forrental)
+
         };
 
 
